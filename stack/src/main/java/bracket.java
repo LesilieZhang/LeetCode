f0449bc578a82646a2c8b44b8c9cf95fcb37aabe
Zion Zhang <canlucingning>
2023-05-08 23:46:01 +0800

    leetcode20.有效括号

--- stack/src/main/java/bracket.java
+++ stack/src/main/java/bracket.java
@@ -19,20 +19,21 @@ public class bracket {
         // 创建一个栈对象
         Stack<String> stack = new Stack<>();
         char arr[]=s.toCharArray();
+        //奇数个括号直接return
         if(arr.length%2!=0){
             return false;
         }
         for(int i=0;i<arr.length;i++){
-            String bracket=String.valueOf(arr[i]);
+            String bracket=String.valueOf(arr[i]);//遍历括号
             if("{".equals(bracket)||"[".equals(bracket)||"(".equals(bracket)){//遇到左括号入栈
                 stack.push(bracket);
             }else{
-                //遇到右括号
+                //遇到右括号，如果栈空说明前面没有左括号，直接false
                 if(stack.isEmpty()){
                     return false;
                 }
                 String right_bracket= String.valueOf(arr[i]);
-                if(!stack.isEmpty()) {
+                if(!stack.isEmpty()) {//如果栈不空，弹出左括号来和右括号做匹配
                     String left_bracket = stack.pop();
                     if (!isMatch(left_bracket, right_bracket)) {
                         isMatch = false;
